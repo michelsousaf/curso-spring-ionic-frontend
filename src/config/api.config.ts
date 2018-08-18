85af36397e696e4356deb0d7759f7b81ad561a2a
Michel Fernandes <michel.sousaf@gmail.com>
2018-08-18 13:21:23 -0300

    Mostrando dados e imagem do cliente para página de profile

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
   baseUrl: "http://localhost:8080",
-  backetBaseUrl: "https://s3.amazonaws.com/curso-spring-ionic-new"
+  bucketBaseUrl: "https://s3.amazonaws.com/curso-spring-ionic-new"
 }
