823dbe755b088cbcb6573919f164888bb975f8a6
Michel Fernandes <michel.sousaf@gmail.com>
2018-08-22 19:38:50 -0300

    Terminando as funcionalidade do carrinho

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -4,6 +4,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CartService } from '../../services/domain/cart.service';
+import { ProdutoDTO } from '../../models/produto.dto';
 
 @IonicPage()
 @Component({
@@ -39,5 +40,21 @@ export class CartPage {
         },
         error => {});
     }
+  }
+
+  removeItem(produto: ProdutoDTO) {
+    this.items = this.cartService.removeProduto(produto).items;
+  }
+   increaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.increaseQuantity(produto).items;
+  }
+   decreaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.decreaseQuantity(produto).items;
+  }
+   total() : number {
+    return this.cartService.total();
+  }
+   goOn() {
+    this.navCtrl.setRoot('CategoriasPage');
   }
 }
