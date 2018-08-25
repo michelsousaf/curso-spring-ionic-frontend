b60177e62aeca2ac4726a42abfe1bdbf1ea556ab
Michel Fernandes <michel.sousaf@gmail.com>
2018-08-25 15:08:11 -0300

    Tela de confirmacao de pedido

---
+++ src/pages/oder-confirmation/oder-confirmation.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { OderConfirmationPage } from './oder-confirmation';
+
+@NgModule({
+  declarations: [
+    OderConfirmationPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(OderConfirmationPage),
+  ],
+})
+export class OderConfirmationPageModule {}
